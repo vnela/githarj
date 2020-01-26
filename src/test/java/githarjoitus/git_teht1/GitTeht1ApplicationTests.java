e2cbf500088f41259dec5889f38f7b1466fec999
unknown <venla.vihersaari@gmail.com>
2020-01-26 10:45:52 +0200

    projekti luotu

---
+++ src/test/java/githarjoitus/git_teht1/GitTeht1ApplicationTests.java
@@ -0,0 +1,13 @@
+package githarjoitus.git_teht1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTeht1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
